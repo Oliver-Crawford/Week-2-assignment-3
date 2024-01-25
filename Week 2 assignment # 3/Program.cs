d98f1112b5cd92af8172777d9cad2e5d78ab161c
Oliver Crawford <oicrawford@gmail.com>
2024-01-25 17:57:35 -0600

    Add project files.

---
+++ Week 2 assignment # 3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_assignment___3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write an application which let user input 2 numbers N, M.
+             * Then, create an array which has M row and Ncolumn and input the elements for those arrays.
+             * Find the row which has the smallest sum of allelements among all rows in the array. */
+
+
+        }
+    }
+}
