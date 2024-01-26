19e755f19ac0225bd42b3e6893bb7124745c9343
Oliver Crawford <oicrawford@gmail.com>
2024-01-25 18:01:40 -0600

    added sanitize method and some stuff in main

--- Week 2 assignment # 3/Program.cs
+++ Week 2 assignment # 3/Program.cs
@@ -13,8 +13,27 @@ namespace Week_2_assignment___3
             /*Write an application which let user input 2 numbers N, M.
              * Then, create an array which has M row and Ncolumn and input the elements for those arrays.
              * Find the row which has the smallest sum of allelements among all rows in the array. */
-
+            Console.WriteLine("Please input number N.");
+            int intN = sanitizeIntInput();
+            Console.WriteLine("Please input number M.");
+            int intM = sanitizeIntInput();
             
+        }
+        static int sanitizeIntInput()
+        {
+            bool isInt = false;
+            string input;
+            int inputInt = 0;
+            while (!isInt)
+            {
+                input = Console.ReadLine();
+                isInt = int.TryParse(input, out inputInt);
+                if (!isInt)
+                {
+                    Console.WriteLine("Invalid input, try again.");
+                }
+            }
+            return inputInt;
         }
     }
 }
