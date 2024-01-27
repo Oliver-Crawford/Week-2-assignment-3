7dbf5b5abf3062432bf1fbe174d556b8c6163330
Oliver Crawford <oicrawford@gmail.com>
2024-01-26 23:58:54 -0600

    Finished Exercise 3

--- Week 2 assignment # 3/Program.cs
+++ Week 2 assignment # 3/Program.cs
@@ -13,11 +13,73 @@ namespace Week_2_assignment___3
             /*Write an application which let user input 2 numbers N, M.
              * Then, create an array which has M row and Ncolumn and input the elements for those arrays.
              * Find the row which has the smallest sum of allelements among all rows in the array. */
-            Console.WriteLine("Please input number N.");
-            int intN = sanitizeIntInput();
-            Console.WriteLine("Please input number M.");
-            int intM = sanitizeIntInput();
+            int intM = 0;
+            int intN = 0;
+            int[,] array;
+            int smallestRowIndex = 0;
+            int smallestRowSum = int.MaxValue;
+            int tempSum = 0;
+            Console.WriteLine("Please input number N that's greater than 0.");
             
+            while (intN <= 0)
+            {
+                intN = sanitizeIntInput();
+                if (intN <= 0)
+                {
+                    Console.WriteLine($"{intN} isn't greater than 0, try again");
+                }
+            }
+            Console.WriteLine("Please input number M that's greater than 0.");
+            while (intM <= 0)
+            {
+                intM = sanitizeIntInput();
+                if (intM <= 0)
+                {
+                    Console.WriteLine($"{intM} isn't greater than 0, try again");
+                }
+            }
+            array = new int[intM, intN];
+            for(int i = 0; i < intM; i++)
+            {
+                for(int a = 0; a < intN; a++)
+                {
+                    Console.WriteLine($"Input a number for position {i}, {a}");
+                    array[i, a] = sanitizeIntInput();
+                }
+            }
+            Console.WriteLine("The array: ");
+            for (int i = 0; i < intM; i++)
+            {
+                for (int a = 0; a < intN; a++)
+                {
+                    if (a == 0)
+                    {
+                        Console.Write($"{array[i, a]}");
+                    } else
+                    {
+                        Console.Write($", {array[i, a]}");
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+            for (int i = 0; i < intM; i++)
+            {
+
+                for (int a = 0; a < intN; a++)
+                {
+                    tempSum += array[i, a];
+                }
+                if (tempSum < smallestRowSum)
+                {
+                    
+                    smallestRowIndex = i;
+                    smallestRowSum = tempSum;
+                }
+                tempSum = 0;
+            }
+            Console.WriteLine($"The index of the row with the smallest sum of all elements is: {smallestRowIndex}");
+            Console.ReadLine();
         }
         static int sanitizeIntInput()
         {
